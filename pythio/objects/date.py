ec28072cea2545661aafdc27e7595032dcc866dd
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-14 15:35:38 +0330

    Update date.py

---
+++ pythio/objects/date.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from . import Object
+
+
+class Date(Object, datetime):
+
+    @classmethod
+    def wrap(cls, raw_object):
+        date_time = datetime.fromtimestamp(raw_object)
+        return super().__new__(cls, *date_time.timetuple()[:6])
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, *args, *kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return str(self)
+
+    def unwrap(self):
+        return int(super().timestamp())
