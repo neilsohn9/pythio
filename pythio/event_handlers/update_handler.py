6b37126e65d9afecbc2fe6ee36c23333a232611f
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-14 18:18:36 +0330

    Update update_handler.py

---
+++ pythio/event_handlers/update_handler.py
@@ -0,0 +1,16 @@
+from .event_handler import EventHandler
+from ..objects import Object
+
+
+class UpdateHandler(EventHandler):
+    can_handle = Object
+
+    def __init__(self, callback, condition=None):
+        super().__init__(callback, condition)
+
+    async def __call__(self, *args, client=None, event=None, **kwargs):
+        if client is not None:
+            kwargs['client'] = client
+        if event is not None:
+            kwargs['update'] = event
+        await super().__call__(*args, **kwargs)
