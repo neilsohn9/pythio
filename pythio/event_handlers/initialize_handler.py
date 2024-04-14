1fe301c6a0e6209cf33970cf3eb2de011cdf67b1
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-14 18:13:08 +0330

    Update initialize_handler.py

---
+++ pythio/event_handlers/initialize_handler.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from .event_handler import EventHandler
+
+
+class InitializeHandler(EventHandler):
+
+    @property
+    def can_handle(self):
+        return InitializeHandler
+
+    def __init__(self, callback):
+        super().__init__(callback)
+
+    async def __call__(self, *args, client=None, event=None, **kwargs):
+        if client is not None:
+            kwargs['client'] = client
+        await super().__call__(*args, **kwargs, time=datetime.now())
