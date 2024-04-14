742c45e29a72c0f06625302a6f5ba905152c3495
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-14 18:09:00 +0330

    Update error_handler.py

---
+++ pythio/event_handlers/error_handler.py
@@ -0,0 +1,15 @@
+from event_handler import EventHandler
+
+
+class ErrorHandler(EventHandler):
+    can_handle = Exception
+
+    def __init__(self, callback, condition=None):
+        super().__init__(callback, condition)
+
+    async def __call__(self, *args, client=None, event=None, **kwargs):
+        if client is not None:
+            kwargs['client'] = client
+        if event is not None:
+            kwargs['error'] = event
+        await super().__call__(*args, **kwargs)
