42aad77601f2ffbbba782fcab1bbb958ab19733f
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-14 18:08:12 +0330

    Update edited_message_handler.py

---
+++ pythio/event_handlers/edited_message_handler.py
@@ -0,0 +1,16 @@
+from update_handler import UpdateHandler
+from ..objects import EditedMessage
+
+
+class EditedMessageHandler(UpdateHandler):
+    can_handle = EditedMessage
+
+    def __init__(self, callback, condition=None):
+        super().__init__(callback, condition)
+
+    def __call__(self, *args, client=None, event=None, **kwargs):
+        if client is not None:
+            kwargs["client"] = client
+        if event is not None:
+            kwargs["edited_message"] = event
+        return super().__call__(*args, **kwargs)
