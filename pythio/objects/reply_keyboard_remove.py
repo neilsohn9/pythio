e47a911442710b4b78dd623569ae1efa3c8af5f1
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-14 17:27:14 +0330

    Update reply_keyboard_remove.py

---
+++ pythio/objects/reply_keyboard_remove.py
@@ -0,0 +1,10 @@
+from . import ReplyKeyboard
+
+
+class ReplyKeyboardRemove(ReplyKeyboard):
+
+    def __init__(self):
+        super().__init__([])
+
+    def __repr__(self):
+        return f'{type(self).__name__}()'
