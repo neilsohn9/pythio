5486510e7a89b3203445680572baf23116f9cbbc
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-14 18:14:20 +0330

    Update message_handler.py

---
+++ pythio/event_handlers/message_handler.py
@@ -0,0 +1,16 @@
+from .update_handler import UpdateHandler
+from ..objects import Message
+
+
+class MessageHandler(UpdateHandler):
+    can_handle = Message
+
+    def __init__(self, callback, condition=None):
+        super().__init__(callback, condition)
+
+    def __call__(self, *args, client=None, event=None, **kwargs):
+        if client is not None:
+            kwargs['client'] = client
+        if event is not None:
+            kwargs['message'] = event
+        return super().__call__(*args, **kwargs)
